b34fa038745d654d5503ab37c8e23d2cc58a9853
xuanxu <xuanxusb@outlook.com>
2021-06-18 21:04:49 +0800

    添加跨域

--- src/main/java/com/rjgc/PestIdentificationGatewayApplication.java
+++ src/main/java/com/rjgc/PestIdentificationGatewayApplication.java
@@ -3,6 +3,10 @@ package com.rjgc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +14,19 @@ public class PestIdentificationGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PestIdentificationGatewayApplication.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 
 }
